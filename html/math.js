4f38fa77790dbaa521260471529c73606333f1b9
DESKTOP-P4UUKAO\student <redcarbb@gmail.com>
2025-09-12 21:40:43 +0800

    0912下課

--- html/math.js
+++ html/math.js
@@ -1,4 +1,12 @@
 export const add = (a, b) => a + b;
 export const subtract = (a, b) => a - b;
-const hello=(x)=> `hello ${x}`
-export default hello
+
+const hello = (x) => `hello ${x}`;
+const version = "V.202250912.1";
+const utils = {
+  add,
+  subtract,
+  hello,
+  version,
+};
+export default utils;
