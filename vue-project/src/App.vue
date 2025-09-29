5c19803190b737c8cebd24add1efc73097803d26
DESKTOP-P4UUKAO\student <redcarbb@gmail.com>
2025-09-29 21:33:24 +0800

    下課

---
+++ vue-project/src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import PropComponent from './components/PropComponent.vue'
+
+const msgT = "I'm Reid"
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <PropComponent
+        :msg="msgT"
+        :msgText="'I am'"
+        :age="'12'"
+        :userName="'Jerry'"
+        :defaultUserName="'John'"
+        :propF="{ msg: 'hello' }"
+        :propG="'success'"
+      />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ vue-project/src/components/PropComponent.vue
@@ -0,0 +1,62 @@
+<script setup>
+const props = defineProps({
+  msg: String,
+  msgText: {
+    type: String,
+    required: true,
+  },
+  age: [String, Number, Object],
+  userName: String,
+  defaultUserName: { type: String, default: 'Mary' },
+  propF: {
+    type: Object,
+    default(rawProps) {
+      return { message: 'hello' }
+    },
+  },
+  propG: {
+    validator(value, props) {
+      return ['success', 'warning', 'danger'].includes(value)
+    },
+  },
+})
+
+const helloWorld = `${props.msgText} ${props.userName}`
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{ `${msgText} ${userName}` }}</h1>
+    <h1 class="green">{{ helloWorld }}</h1>
+    <h1 class="green">{{ age }}</h1>
+    <h1 class="green">{{ defaultUserName }}</h1>
+    <h1 class="green">{{ propF }}</h1>
+    <h1 class="green">{{ propG }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
